02163ce0cc8119af539120da5733456925f980e1
PROPHESSOR <xenteze@gmail.com>
2019-11-30 22:33:27 +0200

    Сделал выводы и переписал нормально
    Сейчас галерея представляет из себя кнопку Fetch images,
    после нажатия на которую ниже появится список картинок 100x100

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,144 +1,44 @@
 import * as WebBrowser from 'expo-web-browser';
 import React, { useState, useEffect } from 'react';
-import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View, FlatList, Button } from 'react-native';
-import { MonoText } from '../components/StyledText';
+import { Image, ScrollView, StyleSheet, View, FlatList, Button } from 'react-native';
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+export default function Gallery() {
+  const [ images, images_set ] = useState([]);
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
+  const fetchImages = async () => {
+    console.log('fetching...');
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
+    const request = await fetch('https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0');
 
-          {/* <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
+    if (request.status !== 200) return console.error(request.status, request.statusText);
 
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload. Really?
-          </Text> */}
-          <Image
-            style={styles.welcomeImage}
-            source={{uri: 'https://i.vimeocdn.com/portrait/58832_300x300.jpg'}}
-            />
-          <Button title="Fetch images" />
-          <Text>Images from Unsplash:</Text>
-          <TestList />
-        </View>
+    const json = await request.json();
 
-        {/* <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView> */}
+    images_set(json);
 
-      {/* <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
+    console.log('OK!');
+  };
 
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View> */}
-      </ScrollView>
-    </View>
-  );
-}
+  return <View style={styles.container}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}>
+      <View>
+        <Button title="Fetch images" onPress={fetchImages} />
 
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-let listItems = [];
-
-function TestList() {
-  const [ images, setImages ] = useState([]);
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      console.log('fetching...');
-
-      const request = await fetch('https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0');
-
-      console.log("Got responce: ", request);
-      
-      const json = await request.json();
-      console.log("Got JSON: ", json);
-
-      setImages(json);
-    };
-
-    fetchImages();
-  }, []);
-
-  return <FlatList 
-    data={images}
-    renderItem={({ item }) => <GalleryItem image={ item } />}
-    keyExtractor={el => el.id}
-    />;
-}
-
-function GalleryItem({ image }) { // TODO: URL
-  return <View>
-    <Image
-      source={{uri: image.urls.thumb}}
-      style={{ width: 50, height: 50 }}/>
-    <Text>An image by { image.user.username }</Text>
+        <FlatList data={images} renderItem={({ item }) => <GalleryItem item={item} />} keyExtractor={item => item.id} />
+      </View>
+    </ScrollView>
   </View>;
 }
 
-function DevelopmentModeNotice() {
-  if (__DEV__)
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. 
-        <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      </Text>
-    );
+// Hide the header offset
+Gallery.navigationOptions = {
+  header: null,
+};
 
-  return (
-    <Text style={styles.developmentModeText}>
-      You are not in development mode: your app will run at full speed.
-    </Text>
-  );
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
+function GalleryItem({ item }) {
+  return <Image source={{ uri: item.urls.thumb }} style={styles.galleryItem} />
 }
 
 const styles = StyleSheet.create({
@@ -146,86 +46,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
     paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
   },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  galleryItem: {
+    width: 100,
+    height: 100
   },
 });
